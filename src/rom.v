5edff24b97b51bd411c02cb1028cffcf8f595520
stranger0virus <61650593+stranger0virus@users.noreply.github.com>
2025-11-19 13:15:48 +0530

    rom v2

--- src/rom.v
+++ src/rom.v
@@ -6,11 +6,11 @@ module rom (
 
 always @(*) begin
     case (addr)
-        4'h0: instr = 8'h12; // LDI 2
-        4'h1: instr = 8'h22; // ADD 2
-        4'h2: instr = 8'h23; // ADD 3
-        4'h3: instr = 8'h70; // JZ 0  (never jumps)
-        4'h4: instr = 8'h60; // JMP 0
+        4'd0: instr = 8'h12; //  2
+        4'd1: instr = 8'h22; //  2
+        4'd2: instr = 8'h23; //  3
+        4'd3: instr = 8'h70; //  0  
+        4'd4: instr = 8'h64; //  4
         default: instr = 8'h00; // NOP
     endcase
 end
