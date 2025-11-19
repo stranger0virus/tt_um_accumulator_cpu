92ff778578dfe255ab6f53a8f01a41869ce5a482
stranger0virus <61650593+stranger0virus@users.noreply.github.com>
2025-11-19 13:17:43 +0530

    tiny_cpu tb v2

--- src/tiny_cpu_tb.v
+++ src/tiny_cpu_tb.v
@@ -5,35 +5,52 @@ module tiny_cpu_tb;
     reg         clk;
     reg         rst_n;
     reg  [7:0]  ui_in;
+    reg  [7:0]  uio_in;
     wire [7:0]  uo_out;
 
-    // Instantiate the CPU
     tiny_cpu uut (
         .clk(clk),
         .rst_n(rst_n),
         .ui_in(ui_in),
-        .uo_out(uo_out)
+        .uo_out(uo_out),
+	.uio_in(uio_in)
     );
 
-    // Clock generation: 10ns period = 100MHz
     always #5 clk = ~clk;
 
     initial begin
-        $dumpfile("tiny_cpu_tb.vcd");
-        $dumpvars(0, tiny_cpu_tb);
 
-        // Initialize signals
-        clk   = 0;
-        rst_n = 0;
-        ui_in = 8'h00;
 
-        // Hold reset for some cycles
-        #20;
-        rst_n = 1;
+    $monitor("Time=%0t | Input = %b | Sel = %b | Output = %b |  | Imm = %b", $time, ui_in, uio_in, uo_out, uut.imm);
+    
+	clk   = 0;
+    rst_n = 0;
+    ui_in = 8'h00;
+	uio_in = 8'h00;
 
-        // Run long enough to execute ROM program
-        #400;
+    #10 rst_n = 1;
+	#5;
+	
+	ui_in = 8'b00100001;
+	uio_in = 8'b00000000;
+	#10;
 
+	ui_in = 8'b00110001;
+	uio_in = 8'b00000000;
+	#10;
+
+    ui_in = 8'b01000010;
+    uio_in = 8'b00000000;
+    #10;
+
+    ui_in = 8'b01010010;
+    uio_in = 8'b00000000;
+    #10;
+
+	ui_in = 8'b00100001;
+	uio_in = 8'b00000001;
+	#300;
+        
         $finish;
     end
 
