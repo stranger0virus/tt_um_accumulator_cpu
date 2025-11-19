0a1a163b88914716432e308b157293317de07dd7
stranger0virus <61650593+stranger0virus@users.noreply.github.com>
2025-11-19 13:14:16 +0530

    tiny_cpu v2

--- src/tiny_cpu.v
+++ src/tiny_cpu.v
@@ -1,57 +1,45 @@
-`timescale 1ns / 1ps
-
-`include "alu.v"
-`include "rom.v"
+`timescale 1ns/1ps
 
 module tiny_cpu (
     input        clk,
     input        rst_n,
     input  [7:0] ui_in,
+    input  [7:0] uio_in,
     output [7:0] uo_out
 );
 
-    // Registers
+   //  Registers
     reg [3:0] pc;
     reg [3:0] acc;
 
     wire [7:0] instr;
-    wire [3:0] opcode = instr[7:4];
-    wire [3:0] imm    = instr[3:0];
-
+    wire [3:0] opcode = ui_in[7:4];
+    reg [3:0] imm;
     wire [3:0] alu_out;
 
-    // Instantiate ROM & ALU
+   //  Instantiate ROM & ALU
     rom u_rom (.addr(pc), .instr(instr));
     alu u_alu (.acc(acc), .imm(imm), .opcode(opcode), .result(alu_out));
 
-    // Sequential logic
+   //  Sequential logic
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             pc  <= 4'd0;
             acc <= 4'd0;
-        end else begin
-            // ACC update
-            case (opcode)
-                4'h1: acc <= imm;        // LDI
-                4'h2: acc <= alu_out;    // ADD
-                4'h3: acc <= alu_out;    // SUB
-                4'h4: acc <= alu_out;    // AND
-                4'h5: acc <= alu_out;    // OR
-                default: acc <= acc;
-            endcase
-
-            // PC logic
-            if (opcode == 4'h6) begin
-                pc <= imm;               // JMP
-            end else if (opcode == 4'h7 && acc == 4'd0) begin
-                pc <= imm;               // JZ
-            end else begin
-                pc <= pc + 1;
-            end
-        end
-    end
-
-    // Output ACC and PC
-    assign uo_out = {pc, acc};
-
+			imm <= 4'd0;
+        end 
+		else begin	
+			if(uio_in[0] == 0) begin
+				imm <= 5;
+				acc <= ui_in[3:0];
+			end
+			else if(uio_in[0] == 1) begin
+				imm <= instr[3:0];
+				acc <= alu_out;
+				pc <= (pc < 4'd5) ? pc + 1 : 4'd0;
+			end
+		end
+	end
+    //  Output ACC and PC
+    assign uo_out = {uio_in[3:0], alu_out};
 endmodule
